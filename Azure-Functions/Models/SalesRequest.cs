844f85eb1d85f904afa40834962748bb993c4810
John Alberto López Hernández <johnlopezhernandez@outlook.com>
2022-07-04 18:00:41 +0200

    Creation Model SalesReport and View Index.

---
+++ Azure-Functions/Models/SalesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Azure_Functions.Models
+{
+    public class SalesRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
