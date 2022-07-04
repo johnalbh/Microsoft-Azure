4a0725a0d0a957f25b93e3767fd8adb96553fea4
John Alberto López Hernández <johnlopezhernandez@outlook.com>
2022-07-04 17:52:50 +0200

    Creación Proyecto Azure inicial

---
+++ Azure-Functions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_Functions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
