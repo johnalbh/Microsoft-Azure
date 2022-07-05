513e7e002bb13498795bd3accfcd2349962ddd9e
John Alberto López Hernández <johnalbh@gmail.com>
2022-07-05 20:48:47 +0200

    Implementation New Trigger to Update DataBase

--- 02-Functions/OnQueueTriggerUpdateDatabase.cs
+++ 02-Functions/OnQueueTriggerUpdateDatabase.cs
@@ -1,4 +1,6 @@
 using System;
+using _02_Functions.Data;
+using _02_Functions.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,11 +9,20 @@ namespace _02_Functions
 {
     public class OnQueueTriggerUpdateDatabase
     {
-        [FunctionName("OnQueueTriggerUpdateDatabase")]
-        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobStorage")]string myQueueItem, ILogger log)
+        private readonly AzureCourseDBContext _db;
+
+        public OnQueueTriggerUpdateDatabase(AzureCourseDBContext db)
         {
-            log.
+            _db = db;
+        }
+        [FunctionName("OnQueueTriggerUpdateDatabase")]
+        public void Run([QueueTrigger("salesrequestinbound", Connection = "AzureWebJobsStorage")]SalesRequest myQueueItem, ILogger log)
+        {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            myQueueItem.Status = "Submitted";
+            _db.SalesRequests.Add(myQueueItem);
+            _db.SaveChanges();
         }
     }
 }
---
+++ 02-Functions/Startup.cs
@@ -0,0 +1,21 @@
+﻿using _02_Functions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using System;
+using _02_Functions.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace _02_Functions
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
+            builder.Services.AddDbContext<AzureCourseDBContext>(opt => opt.UseSqlServer(connectionString));
+            builder.Services.BuildServiceProvider();
+        }
+    }
+}
